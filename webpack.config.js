c39c26b0afc48803fc85b3546c02afa7a912d306
Raul Marchan Catala <alountk@gmail.com>
2021-09-28 23:51:45 +0200

    add exclude

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: './app/index.jsx',
+  entry: "./app/index.jsx",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
   },
   module: {
     rules: [
-      { test: /\.(js|mjs|jsx|ts|tsx)$/, use: 'babel-loader' },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      {
+        test: /\.(js|mjs|jsx|ts|tsx)$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        exclude: path.resolve(__dirname, "node_modules"),
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
-  mode: 'development',
+  mode: "development",
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html',
+      template: "app/index.html",
     }),
   ],
 };
